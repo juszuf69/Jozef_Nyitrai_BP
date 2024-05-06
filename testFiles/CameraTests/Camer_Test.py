29a21d0bdba8ef5ab94c97105d3975712281413d
juszuf69 <xnyitrai@stuba.sk>
2024-05-06 19:33:14 +0200

    change in color picture

--- testFiles/CameraTests/Camer_Test.py
+++ testFiles/CameraTests/Camer_Test.py
@@ -23,7 +23,7 @@ def convert_image(image):
     return thresh1
 
 
-def find_centroid(image):
+def find_centroid(image, color_image):
     # Find all contours in frame
     contours, hierarchy = cv2.findContours(image.copy(), 1, cv2.CHAIN_APPROX_NONE)
     # Find the largest contour and display the centroid
@@ -31,13 +31,13 @@ def find_centroid(image):
         # Find the largest contour area and image moments
         c = max(contours, key=cv2.contourArea)
         # display the largest contour
-        cv2.drawContours(image, [c], -1, (0, 255, 0), 2)
+        cv2.drawContours(color_image, [c], -1, (0, 255, 0), 2)
         M = cv2.moments(c)
         # Find x-axis centroid using image moments
         cx = int(M['m10'] / M['m00'])
         cy = int(M['m01'] / M['m00'])
-        cv2.circle(image, (cx, cy), 5, (0, 0, 255), -1)
-        return image
+        cv2.circle(color_image, (cx, cy), 5, (0, 0, 255), -1)
+        return color_image
 
 
 if __name__ == '__main__':
@@ -52,30 +52,30 @@ if __name__ == '__main__':
     cv2.imshow('Original Straight Image', straight_image)
     cv2.imshow('Original Stop Image', stop_image)
     # Resize the images
-    Lturn_image = resize_image(Lturn_image)
-    Rturn_image = resize_image(Rturn_image)
-    straight_image = resize_image(straight_image)
-    stop_image = resize_image(stop_image)
+    Lturn_image_c = resize_image(Lturn_image)
+    Rturn_image_c = resize_image(Rturn_image)
+    straight_image_c = resize_image(straight_image)
+    stop_image_c = resize_image(stop_image)
     # Display the resized images
-    cv2.imshow('Resized Left Turn Image', Lturn_image)
-    cv2.imshow('Resized Right Turn Image', Rturn_image)
-    cv2.imshow('Resized Straight Image', straight_image)
-    cv2.imshow('Resized Stop Image', stop_image)
+    cv2.imshow('Resized Left Turn Image', Lturn_image_c)
+    cv2.imshow('Resized Right Turn Image', Rturn_image_c)
+    cv2.imshow('Resized Straight Image', straight_image_c)
+    cv2.imshow('Resized Stop Image', stop_image_c)
     # Convert the images to use in the contour detection
-    Lturn_image = convert_image(Lturn_image)
-    Rturn_image = convert_image(Rturn_image)
-    straight_image = convert_image(straight_image)
-    stop_image = convert_image(stop_image)
+    Lturn_image = convert_image(Lturn_image_c)
+    Rturn_image = convert_image(Rturn_image_c)
+    straight_image = convert_image(straight_image_c)
+    stop_image = convert_image(stop_image_c)
     # Display the converted images
     cv2.imshow('Converted Left Turn Image', Lturn_image)
     cv2.imshow('Converted Right Turn Image', Rturn_image)
     cv2.imshow('Converted Straight Image', straight_image)
     cv2.imshow('Converted Stop Image', stop_image)
     # Find the centroid of the images
-    Lturn_image = find_centroid(Lturn_image)
-    Rturn_image = find_centroid(Rturn_image)
-    straight_image = find_centroid(straight_image)
-    stop_image = find_centroid(stop_image)
+    Lturn_image = find_centroid(Lturn_image, Lturn_image_c)
+    Rturn_image = find_centroid(Rturn_image, Rturn_image_c)
+    straight_image = find_centroid(straight_image, straight_image_c)
+    stop_image = find_centroid(stop_image, stop_image_c)
     # Display the images with the centroid
     cv2.imshow('Left Turn Image with Centroid', Lturn_image)
     cv2.imshow('Right Turn Image with Centroid', Rturn_image)
