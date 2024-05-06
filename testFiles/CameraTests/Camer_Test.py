753af4d00c6400ab543e92a5942aa5120f8775a1
juszuf69 <xnyitrai@stuba.sk>
2024-05-06 19:40:54 +0200

    change in pic save

--- testFiles/CameraTests/Camer_Test.py
+++ testFiles/CameraTests/Camer_Test.py
@@ -5,6 +5,8 @@ RIGHT_TURN_IMAGE_PATH = 'test_Pictures/Rturn_image.jpg'
 STRAIGHT_IMAGE_PATH = 'test_Pictures/straight_line_image.jpg'
 STOP_IMAGE_PATH = 'test_Pictures/stop_line_image.jpg'
 
+OUT_PATH = 'test_Pictures/utput/'
+
 
 def resize_image(image):
     # Resize the image
@@ -46,11 +48,15 @@ if __name__ == '__main__':
     Rturn_image = cv2.imread(RIGHT_TURN_IMAGE_PATH)
     straight_image = cv2.imread(STRAIGHT_IMAGE_PATH)
     stop_image = cv2.imread(STOP_IMAGE_PATH)
-    # Display the original images
+    # Display and save the original images
     cv2.imshow('Original Left Turn Image', Lturn_image)
     cv2.imshow('Original Right Turn Image', Rturn_image)
     cv2.imshow('Original Straight Image', straight_image)
     cv2.imshow('Original Stop Image', stop_image)
+    cv2.imwrite(OUT_PATH + 'Original_Lturn_image.jpg', Lturn_image)
+    cv2.imwrite(OUT_PATH + 'Original_Rturn_image.jpg', Rturn_image)
+    cv2.imwrite(OUT_PATH + 'Original_straight_image.jpg', straight_image)
+    cv2.imwrite(OUT_PATH + 'Original_stop_image.jpg', stop_image)
     # Resize the images
     Lturn_image_c = resize_image(Lturn_image)
     Rturn_image_c = resize_image(Rturn_image)
@@ -61,6 +67,10 @@ if __name__ == '__main__':
     cv2.imshow('Resized Right Turn Image', Rturn_image_c)
     cv2.imshow('Resized Straight Image', straight_image_c)
     cv2.imshow('Resized Stop Image', stop_image_c)
+    cv2.imwrite(OUT_PATH + 'Resized_Lturn_image.jpg', Lturn_image_c)
+    cv2.imwrite(OUT_PATH + 'Resized_Rturn_image.jpg', Rturn_image_c)
+    cv2.imwrite(OUT_PATH + 'Resized_straight_image.jpg', straight_image_c)
+    cv2.imwrite(OUT_PATH + 'Resized_stop_image.jpg', stop_image_c)
     # Convert the images to use in the contour detection
     Lturn_image = convert_image(Lturn_image_c)
     Rturn_image = convert_image(Rturn_image_c)
@@ -71,6 +81,10 @@ if __name__ == '__main__':
     cv2.imshow('Converted Right Turn Image', Rturn_image)
     cv2.imshow('Converted Straight Image', straight_image)
     cv2.imshow('Converted Stop Image', stop_image)
+    cv2.imwrite(OUT_PATH + 'Converted_Lturn_image.jpg', Lturn_image)
+    cv2.imwrite(OUT_PATH + 'Converted_Rturn_image.jpg', Rturn_image)
+    cv2.imwrite(OUT_PATH + 'Converted_straight_image.jpg', straight_image)
+    cv2.imwrite(OUT_PATH + 'Converted_stop_image.jpg', stop_image)
     # Find the centroid of the images
     Lturn_image = find_centroid(Lturn_image, Lturn_image_c)
     Rturn_image = find_centroid(Rturn_image, Rturn_image_c)
@@ -81,6 +95,10 @@ if __name__ == '__main__':
     cv2.imshow('Right Turn Image with Centroid', Rturn_image)
     cv2.imshow('Straight Image with Centroid', straight_image)
     cv2.imshow('Stop Image with Centroid', stop_image)
+    cv2.imwrite(OUT_PATH + 'Centroid_Lturn_image.jpg', Lturn_image)
+    cv2.imwrite(OUT_PATH + 'Centroid_Rturn_image.jpg', Rturn_image)
+    cv2.imwrite(OUT_PATH + 'Centroid_straight_image.jpg', straight_image)
+    cv2.imwrite(OUT_PATH + 'Centroid_stop_image.jpg', stop_image)
     # Wait for user to press a key
     cv2.waitKey(0)
     cv2.destroyAllWindows()
