9ff9d74aee74e32563942e52cf4e1aa23c428af1
juszuf69 <xnyitrai@stuba.sk>
2024-05-09 19:26:40 +0200

    change

--- algorithms/Camera_Follow_Line.py
+++ algorithms/Camera_Follow_Line.py
@@ -156,7 +156,7 @@ def get_left_right_points(image):
         rightmost = tuple(largest_contour[largest_contour[:, :, 0].argmax()][0])
         return leftmost[0], rightmost[0]
     else:
-        return 'noLine', 'noLine'
+        return 0, 191
 
 
 def followLine(car, speed):
@@ -177,8 +177,6 @@ def followLine(car, speed):
         if leftmost == 0 and rightmost == 191 and not first:
             car.stop()
             break
-        elif leftmost == 'noLine' and rightmost == 'noLine' and not first:
-            car.backward(speed)
         first = False
         # Find centroid x of the line
         centroid_x = find_centroid(image)
